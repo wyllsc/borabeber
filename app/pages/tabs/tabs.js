aa6762b1378e733bdc934528aae0650ed953fd96
Wylliam SantClair <wyllsc@gmail.com>
2016-02-19 18:44:39 -0200

    Ajustes de Template

--- app/pages/tabs/tabs.js
+++ app/pages/tabs/tabs.js
@@ -1,7 +1,7 @@
 import {Page} from 'ionic/ionic';
-import {Page1} from '../page1/page1';
-import {Page2} from '../page2/page2';
-import {Page3} from '../page3/page3';
+import {Promocoes} 	from '../promocoes/promocoes';
+import {Page2} 		from '../page2/page2';
+import {Page3} 		from '../page3/page3';
 
 @Page({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -9,7 +9,7 @@ import {Page3} from '../page3/page3';
 
 export class TabsPage {
 	constructor() {
-	    this.tab1Root = Page1;
+	    this.tabPromocoes = Promocoes;
 	    this.tab2Root = Page2;
 	    this.tab3Root = Page3;
 	}
